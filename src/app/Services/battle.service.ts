235615239045d896b754b2175a065acaa700b24a
Connor Jones <ConnorLeeJones@gmail.com>
2019-10-31 15:23:02 -0400

    Battle progress

--- src/app/Services/battle.service.ts
+++ src/app/Services/battle.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Player } from '../Classes/player';
+import { PlayerForm } from '../Classes/player-form';
+import { BattleModel } from '../Classes/battle-model';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -9,16 +12,20 @@ import { Player } from '../Classes/player';
 })
 export class BattleService {
   url: string;
+  battle: BattleModel;
 
   constructor(private http: HttpClient) { 
     this.url = environment.baseUrl + '/battles';
   }
 
 
-  newBattle(gameId: number){
-    console.log(gameId);
-    return this.http.post(this.url, gameId).subscribe();
+  newBattle(playerForm : PlayerForm){
+    //console.log(gameId);
+    return this.http.post(this.url, playerForm);
+    //return this.battle;
+    
 
-  }
+      }
+  
 
 }
